32675b87268c9886d785e4b41b853bbc55cbe97f
udo-nabe <yu_w_main@yahoo.co.jp>
2026-08-10 11:12:44 +0900

    組み込み関数と未定義に値を表すRuntimeValueを追加 組み込み関数を表すBuiltinFunctionValue、未定義を表すUndefinedValueを追加。

---
+++ src/main/java/io/github/udonabe/donabe/runtime/value/UndefinedValue.java
@@ -0,0 +1,25 @@
+package io.github.udonabe.donabe.runtime.value;
+
+import io.github.udonabe.donabe.CompileException;
+import io.github.udonabe.donabe.runtime.InterpreterException;
+
+/**
+ * この値に対して行う操作は全て意味解析器で弾くべきであるため、
+ * いかなる操作でもIllegalStateExceptionが送出される。
+ */
+public record UndefinedValue() implements RuntimeValue<Void> {
+    @Override
+    public Void value() {
+        throw new IllegalStateException("Undefined identifier.");
+    }
+
+    @Override
+    public String typeName() {
+        throw new IllegalStateException("Undefined identifier.");
+    }
+
+    @Override
+    public String display() {
+        throw new IllegalStateException("Undefined identifier.");
+    }
+}
