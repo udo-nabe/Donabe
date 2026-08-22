4967ebf55eea4b603526a660e9cdb6004afd5cc0
udo-nabe <yu_w_main@yahoo.co.jp>
2026-08-22 12:26:17 +0900

    単項演算子の命令を追加 plus, minus, not命令を追加。

--- src/main/java/io/github/udonabe/donabe/ir/instruction/Instruction.java
+++ src/main/java/io/github/udonabe/donabe/ir/instruction/Instruction.java
@@ -1,5 +1,5 @@
 package io.github.udonabe.donabe.ir.instruction;
 
-public sealed interface Instruction permits Add, Call, Div, Equal, Greater, GreaterEqual, Jmp, JmpFalse, JmpTrue, LabelNop, Less, LessEqual, Load, LoadLocal, Mul, Nop, Pop, Push, Return, Store, StoreLocal, Sub, VoidReturn {
+public sealed interface Instruction permits Add, Call, Div, Equal, Greater, GreaterEqual, Jmp, JmpFalse, JmpTrue, LabelNop, Less, LessEqual, Load, LoadLocal, Minus, Mul, Nop, Not, Plus, Pop, Push, Return, Store, StoreLocal, Sub, VoidReturn {
 
 }
---
+++ src/main/java/io/github/udonabe/donabe/ir/instruction/Minus.java
@@ -0,0 +1,4 @@
+package io.github.udonabe.donabe.ir.instruction;
+
+public record Minus() implements Instruction {
+}
