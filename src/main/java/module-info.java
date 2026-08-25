1cf2d0c4037c6f09eff45ec7438dba58751fb4bc
udo-nabe <yu_w_main@yahoo.co.jp>
2026-08-25 16:13:01 +0900

    モジュールを導入

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module io.github.udonabe.donabe {
+    requires ch.qos.logback.classic;
+    requires ch.qos.logback.core;
+    requires info.picocli;
+    requires org.apache.commons.lang3;
+    requires org.slf4j;
+}
